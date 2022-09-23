b8c4fce07c9cf811e2363a43ac094b8276fa1c12
jim <jimh@datagrove.com>
2022-09-23 03:34:00 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WordprocessingDocument wordDoc = WordprocessingDocument.Open("./test.docx", false);
+        DocumentFormat.OpenXml.Wordprocessing.Body body
+    = wordDoc.MainDocumentPart.Document.Body;
+        var totaltext = body.InnerText;
+        Console.WriteLine(totaltext);
+    }
+}
